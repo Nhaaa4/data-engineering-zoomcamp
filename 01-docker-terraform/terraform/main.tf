fad9c3ec35cc765dffa5a767b3d8b13712bb3c52
Nhaaa4 <panahsovan51@gmail.com>
2026-02-06 15:59:16 +0700

    Week 1

---
+++ 01-docker-terraform/terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "learning-bucket" {
+  name          = var.gcs_dataset_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "learning_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
---
+++ 01-docker-terraform/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/terraform-learning-486607-8570d51183ea.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "terraform-learning-486607"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "learning_dataset"
+}
+
+variable "gcs_dataset_name" {
+  description = "My Storage Bucket Name"
+  default     = "terraform-learning-486607-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
